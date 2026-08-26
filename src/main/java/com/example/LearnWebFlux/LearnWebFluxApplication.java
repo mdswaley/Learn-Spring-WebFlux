f451df0e27416ba2812a0c02d2d489f2f0ed3c7b
mdswaley <mdswaley075@gmail.com>
2026-08-26 15:54:12 +0530

    first commit

---
+++ src/main/java/com/example/LearnWebFlux/LearnWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearnWebFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LearnWebFlux/LearnWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearnWebFlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
