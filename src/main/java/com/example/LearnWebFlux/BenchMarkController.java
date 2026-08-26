4d8a3d98fc8bd4264953954161f0cdb7c373852c
mdswaley <mdswaley075@gmail.com>
2026-08-26 16:27:22 +0530

    without virtual thread

---
+++ src/main/java/com/example/LearnWebFlux/BenchMarkController.java
@@ -0,0 +1,20 @@
+package com.example.LearnWebFlux;
+
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/benchmark")
+public class BenchMarkController {
+
+    @SneakyThrows
+    @GetMapping("/users/{id}")
+    public String getUser(@PathVariable Long id){
+        Thread.sleep(1000);
+        return "full users : "+id;
+
+    }
+}
