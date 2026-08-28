93eae717ac0fd49ec29a2dab1e86ddf93ac36426
mdswaley <mdswaley075@gmail.com>
2026-08-28 17:05:12 +0530

    add webFlux dependency and create entity

---
+++ src/main/java/com/example/LearnWebFlux/Entity/User.java
@@ -0,0 +1,35 @@
+package com.example.LearnWebFlux.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Table("users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    private Long id;
+
+    private String name;
+    private String email;
+    private String role;
+
+    @CreatedDate
+    @Column("created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column("updated_at")
+    private LocalDateTime updatedAt;
+}
