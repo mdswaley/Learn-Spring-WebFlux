9c5f7f3044fcb89ee0c9f243c1b6846f51b55bd4
mdswaley <mdswaley075@gmail.com>
2026-08-30 20:05:17 +0530

    EnableR2dbcAuditing for automatically fill fields like createdAt etc

--- src/main/java/com/example/LearnWebFlux/LearnWebFluxApplication.java
+++ src/main/java/com/example/LearnWebFlux/LearnWebFluxApplication.java
@@ -2,8 +2,10 @@ package com.example.LearnWebFlux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 @SpringBootApplication
+@EnableR2dbcAuditing
 public class LearnWebFluxApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,14 @@ public class LearnWebFluxApplication {
 	}
 
 }
+/*
+@EnableR2dbcAuditing enables automatic auditing of database entities when you're using Spring Data R2DBC.
+
+In simple terms, it allows Spring to automatically fill fields like:
+
+createdAt → when the record was created
+updatedAt → when the record was last modified
+createdBy → who created it
+updatedBy → who modified it
+
+*/
