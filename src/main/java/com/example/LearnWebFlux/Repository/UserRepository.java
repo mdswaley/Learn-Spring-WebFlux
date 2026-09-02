0a8c3610243bad7ccbe41a438e49c97238c0f7c3
mdswaley <mdswaley075@gmail.com>
2026-09-02 18:06:27 +0530

    delete unnecessary imports

--- src/main/java/com/example/LearnWebFlux/Repository/UserRepository.java
+++ src/main/java/com/example/LearnWebFlux/Repository/UserRepository.java
@@ -1,9 +1,6 @@
 package com.example.LearnWebFlux.Repository;
 
-import com.example.LearnWebFlux.DTO.UserResponse;
 import com.example.LearnWebFlux.Entity.User;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
