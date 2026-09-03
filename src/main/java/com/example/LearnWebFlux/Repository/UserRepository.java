b79adab725a15fce577cacd02176db6c0d0eae45
mdswaley <mdswaley075@gmail.com>
2026-09-03 10:56:45 +0530

    ReactiveSortingRepository having findAll method and sort then according to your requirement

--- src/main/java/com/example/LearnWebFlux/Repository/UserRepository.java
+++ src/main/java/com/example/LearnWebFlux/Repository/UserRepository.java
@@ -2,8 +2,9 @@ package com.example.LearnWebFlux.Repository;
 
 import com.example.LearnWebFlux.Entity.User;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 import reactor.core.publisher.Mono;
 
-public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+public interface UserRepository extends ReactiveCrudRepository<User, Long>, ReactiveSortingRepository<User, Long> {
     Mono<Boolean> existsByEmail(String email);
 }
