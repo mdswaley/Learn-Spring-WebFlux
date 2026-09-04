4c5f46760cd929a2f1bb845789b785354341a3cf
mdswaley <mdswaley075@gmail.com>
2026-09-04 12:57:53 +0530

    implements ProductService

---
+++ src/main/java/com/example/LearnWebFlux/Service/ProductServiceImp.java
@@ -0,0 +1,34 @@
+package com.example.LearnWebFlux.Service;
+
+import com.example.LearnWebFlux.Config.WebClientConfig;
+import com.example.LearnWebFlux.DTO.Product;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductServiceImp implements ProductService{
+
+    private final WebClient productWebClient;
+
+    @Override
+    public Mono<Product> getProductById(Long id) {
+
+        return productWebClient.get()
+                .uri("/{id}", id)
+                .retrieve()
+                .bodyToMono(Product.class);
+    }
+
+    @Override
+    public Flux<Product> getAllProducts() {
+        return productWebClient.get()
+                .retrieve()
+                .bodyToFlux(Product.class);
+    }
+}
